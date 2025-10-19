dcc7d54025d5b89d06f86b3e8fe64a047278a3ac
lucasfgaldinos <lucasfgaldinos@outlook.com>
2025-10-19 14:24:13 -0300

    config: Configuração inicial do projeto

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { Home } from './screens/home';
+
+// biome-ignore lint: false positive
+createRoot(document.getElementById('root')!).render(
+	<StrictMode>
+		<Home />
+	</StrictMode>,
+);
