c544352cd18d67a768739f428d43e4a7e8a8eb61
lucasfgaldinos <lucasfgaldinos@outlook.com>
2025-10-20 17:30:49 -0300

    style: Ajuste de responsividade no Header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,11 +2,11 @@ import { Button } from '../button';
 
 export function Header() {
 	return (
-		<header className="px-4 py-4 shadow sticky top-0 left-0">
+		<header className="px-4 py-4 shadow fixed w-full top-0 left-0 backdrop-blur-lg bg-white/70">
 			<div className="max-w-7xl flex justify-between mx-auto">
 				<p className="gradient-text font-medium text-xl">Logo Goes Here</p>
 
-				<nav className="text-dark font-medium text-lg flex items-center justify-center gap-8">
+				<nav className="text-dark font-medium text-lg items-center justify-center gap-8 hidden lg:flex">
 					<button
 						className="header-link cursor-pointer transition-all duration-500"
 						type="button"
